ddb40bc249bf82f9194bdad37e5cc8ac7e56159b
kashoobest-droid <kashoobest@gmail.com>
2026-01-19 13:35:25 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_CONFIG = {
-  url: 'https://your-project-id.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kdGNpem9uZW5tcWd6YXhlYWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4MTI5NDIsImV4cCI6MjA4NDM4ODk0Mn0.S3towpQzNT9kBlJ557fSJWqWF7XC9pQE6WiTmvs0FkY'
-};
